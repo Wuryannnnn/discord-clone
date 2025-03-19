5defa8bbe0c7d6dd2f5ef48d5abc67e5b08b9c32
wu rui <wurui@wus-MacBook-Pro.local>
2025-03-19 10:38:50 -0700

    add server feature

--- components/models/initial-modal.tsx
+++ components/models/initial-modal.tsx
@@ -8,6 +8,12 @@ import {zodResolver} from "@hookform/resolvers/zod"
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+import { FileUpload } from "../file-upload";
+
+import axios from "axios"
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -20,6 +26,11 @@ const formSchema = z.object({
 
 
 export const InitialModal = () =>{
+    const [isMounted, setIsMounted] = useState(false);
+    const router = useRouter();
+    useEffect(() =>{
+        setIsMounted(true);
+    },[]);
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues:{
@@ -31,7 +42,19 @@ export const InitialModal = () =>{
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try{
+            await axios.post("/api/servers", values);
+            form.reset();
+            router.refresh();
+            window.location.reload();
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    if(!isMounted){
+        return null;
     }
 
     return (
@@ -49,7 +72,17 @@ export const InitialModal = () =>{
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField control={form.control} name="imageUrl" render={({field}) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <FileUpload 
+                                                endpoint = "serverImage"
+                                                value={field.value}
+                                                onChange={field.onChange}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}/>
                             </div>
                             <FormField control={form.control} name="name" render={({field}) => (
                                 <FormItem>
@@ -57,6 +90,7 @@ export const InitialModal = () =>{
                                     <FormControl>
                                         <Input disabled = {isLoading} className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" placeholder="Enter server name" {...field}/>
                                     </FormControl>
+                                    <FormMessage />
                                 </FormItem>
                             )}/>
                                 
@@ -64,7 +98,7 @@ export const InitialModal = () =>{
                         </div>
 
                         <DialogFooter className="bg-gray-100 px-6 py-4">
-                            <Button disabled={isLoading}>
+                            <Button variant="primary" disabled={isLoading}>
                                 Create
                             </Button>
                         </DialogFooter>
---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ["fwpcy3lthf.ufs.sh"], // 这里添加你的图片主机名
+    },
+  };
+  
+  module.exports = nextConfig;
+  
