feb4ca6af09419afa93b95e2fe29d70359589c29
wu rui <wurui@wus-MacBook-Pro.lan>
2025-04-27 23:04:31 -0700

    feat create-channel

--- components/models/create-channel-model.tsx
+++ components/models/create-channel-model.tsx
@@ -5,28 +5,42 @@ import {useForm} from "react-hook-form";
 import * as z from "zod";
 import {zodResolver} from "@hookform/resolvers/zod"
 
+import qs from "query-string";
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
-
+import { useParams } from "next/navigation";
 import axios from "axios"
 import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
+} from '@/components/ui/select'
+import { ChannelType } from "@prisma/client";
 
 const formSchema = z.object({
     name: z.string().min(1, {
-        message: "Server name is required."
+        message: "Channel name is required."
+    }).refine((name) => name !== "general", {
+        message: "Channel name cannot be 'general'"
     }),
+    type: z.nativeEnum(ChannelType)
 })
 
 export const CreateChannelModal = () => {
     const {isOpen, onClose, type, mounted} = useModal();
     const router = useRouter();
+    const params = useParams();
 
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues:{
             name:"",
+            type: ChannelType.TEXT,
         }
     });
 
@@ -34,7 +48,13 @@ export const CreateChannelModal = () => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try{
-            await axios.post("/api/servers", values);
+            const url = qs.stringifyUrl({
+                url: "/api/channels",
+                query:{
+                    serverId: params?.serverId,
+                }
+            });
+            await axios.post(url, values);
             form.reset();
             router.refresh();
             onClose();
@@ -70,6 +90,30 @@ export const CreateChannelModal = () => {
                                     <FormControl>
                                         <Input disabled={isLoading} className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" placeholder="Enter channel name" {...field}/>
                                     </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}/>
+                            <FormField control={form.control} name="type" render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Channel Type</FormLabel>
+                                    <Select
+                                        disabled={isLoading}
+                                        onValueChange={field.onChange}
+                                        defaultValue={field.value}
+                                    >
+                                        <FormControl>
+                                            <SelectTrigger className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none">
+                                                <SelectValue placeholder="Select a channel type" />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            {Object.values(ChannelType).map((type) => (
+                                                <SelectItem key={type} value={type} className="capitalize">
+                                                    {type.toLowerCase()}
+                                                </SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>
                                     <FormMessage />
                                 </FormItem>
                             )}/>
